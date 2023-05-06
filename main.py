b444073d2bf9d3bb4c7389e14abcf8c39e62811a
FunnySweeter <34790257+FunnySweeter@users.noreply.github.com>
2023-05-07 02:26:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
+from keras.utils import np_utils
 from keras import backend as K
 
 import numpy
