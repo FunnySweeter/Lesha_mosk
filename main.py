7a6d4d97fafe70d47a996a4c961d5ab34efd5c10
FunnySweeter <34790257+FunnySweeter@users.noreply.github.com>
2023-05-07 13:01:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,12 @@ from tensorflow import random
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 num_classes = 10
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32')/255
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_train = x_train.astype('float32')
+x_train = x_train/255
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype('float32')/255
+x_test = x_test.astype('float32')
+x_test = x_test/255
 input_shape = (28, 28, 1)
 
 y_train = keras.utils.to_categorical(y_train, num_classes)
@@ -26,21 +30,27 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 print("Размерность x_train:", x_train.shape[0])
 
-batch_size = 100
+batch_size = 128
 epochs = 100
 
 model = Sequential(
    [
-       Conv2D(32, (3, 3), activation="relu", input_shape=input_shape),
-       Conv2D(64, (3, 3), activation="relu"),
+       Conv2D(4, (3, 3), activation="relu", input_shape=input_shape),
+       Conv2D(8, (3, 3), activation="relu"),
+       Conv2D(16, (3, 3), activation="relu"),
+       Conv2D(32, (3, 3), activation="relu"),
        MaxPooling2D(pool_size=(2, 2)),
 
        Dropout(0.25),
        Flatten(),
 
-       Dense(512, "relu"),
-       Dropout(0.25),
        Dense(256, "relu"),
+       Dropout(0.5),
+       Dense(256, "relu"),
+       Dropout(0.5),
+       Dense(256, "relu"),
+       Dropout(0.25),
+       Dense(128, "relu"),
        Dropout(0.25),
        Dense(num_classes, "softmax")
    ]
